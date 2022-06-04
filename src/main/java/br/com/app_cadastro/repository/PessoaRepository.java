908908d05795d822eb70d70bb54f508328d5ec5d
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-06-03 22:32:56 -0300

    imp: Notas de aula 220603 (2)

--- src/main/java/br/com/app_cadastro/repository/PessoaRepository.java
+++ src/main/java/br/com/app_cadastro/repository/PessoaRepository.java
@@ -1,16 +1,14 @@
 package br.com.app_cadastro.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import br.com.app_cadastro.domain.entity.Pessoa;
 
 @Repository
 public interface  PessoaRepository  extends JpaRepository<Pessoa, Long>{
-
-
-	List<Pessoa> findByNome(String nome);
+	
+	Page<Pessoa> findByNome(String nome, Pageable pageable);
 
 }
